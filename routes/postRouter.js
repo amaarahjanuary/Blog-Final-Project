a5e1d1f7b9ebae0d787bcc0d23c4654e8668a45a
Amaarah January <amaarahjanuary456@gmail.com>
2022-03-25 09:40:38 +0200

    removed contact

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require('../middleware/auth')
 const { getPost } = require('../middleware/finders')
 
-// Getting All
+// Getting All posts
 router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
