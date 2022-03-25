73aecb6db38e2bd4a0ca1ce3413047be50739b02
Amaarah January <amaarahjanuary456@gmail.com>
2022-03-25 09:39:24 +0200

    removed contact

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -33,6 +33,7 @@ router.post('/', auth, async (req, res, next) => {
     });
   try {
     const newPost = await post.save();
+    console.log()
     res.status(201).json(newPost);
   } catch (err) {
     res.status(400).json({ msg: err.message });
